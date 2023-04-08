ac23c31702990b61db53ce4e45756c935dd12110
lamoouuuuuur <nastyasavi9a@gmail.com>
2023-04-08 20:37:22 +0300

    day-01

---
+++ 01_Day_Introduction/datatypes.js
@@ -0,0 +1,12 @@
+// Point 5, 6, 7, 8, 9, 10
+
+let firstName = 'Nikolay';
+let secondName = 'Bondarenko';
+let isMarried = false;
+let country = 'Russia';
+let age = 20;
+
+console.log(
+    `I am ${age} years old\nYou are ${age + 10} years old`
+)
+
---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,5 @@
+// Point 4
+let stringVariable = 'Hello';
+let boolVariable = true;
+let undefinedVariable = undefined;
+let nullVariable = null
